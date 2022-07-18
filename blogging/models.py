2ae557aa1b994000501295af3256661bb97ab8c3
Philip Behrend <phbehren@microsoft.com>
2022-07-17 22:44:22 -0700

    L07 assignment

--- blogging/models.py
+++ blogging/models.py
@@ -2,10 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    #posts = models.ForeignKey(Post, blank=True, related_name='categories', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=128)
-    text = models.TextField(blank=True)
+    category = models.ManyToManyField(Category)
     author = models.ForeignKey(User, on_delete=models.CASCADE) # if user is deleted, delete all associated posts
+    text = models.TextField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True) # Set field to current time when post is added
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True) # null means unpublished 
@@ -13,13 +25,4 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Category(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'Categories'
